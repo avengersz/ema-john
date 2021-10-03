ee1c3b480df178d535ca44d2c6baedd46bba4bbc
avengersz <mohaamir553@gmail.com>
2021-10-03 12:47:36 +0600

    cart updated

--- src/components/PlaceOrder/PlaceOrder.js
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import img from '../../images/giphy.gif';
 
 const PlaceOrder = () => {
     return (
         <div>
-            
+            <img src={img} alt="" />
         </div>
     );
 };
